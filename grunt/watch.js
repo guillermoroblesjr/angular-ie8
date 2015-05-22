0519b4b4f055ae4208f3c129a45f8c23dcf4ab18
Guillermo Robles Jr <grobles4@wm.com>
2015-05-22 10:39:29 -0500

    removed watch directory from grunt task

--- grunt/watch.js
+++ grunt/watch.js
@@ -2,7 +2,6 @@ module.exports = {
     src: {
       files: [
         // 'lib/*.js', 'css/**/*.scss', '!lib/dontwatch.js',
-        'OPUSConfirmationsTrunk_v2.0/opus-confirmation-web/WebContent/**'
       ],
       tasks: ['copy_to_temp:main'],
     }
